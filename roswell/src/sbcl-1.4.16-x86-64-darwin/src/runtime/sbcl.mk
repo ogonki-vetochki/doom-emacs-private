c8f97c3ac7cee9e900d8b4ab2690734f506943b8
Nikita Shatov <xamenrax@gmail.com>
2019-02-02 20:32:48 +0300

    Remove Roswell configuration

--- roswell/src/sbcl-1.4.16-x86-64-darwin/src/runtime/sbcl.mk
+++
@@ -1,8 +0,0 @@
-CC=gcc
-LD=ld
-CFLAGS=-g -Wall -O2 -fdollars-in-identifiers -mmacosx-version-min=10.6 -D_DARWIN_USE_64_BIT_INODE -arch x86_64 -fno-omit-frame-pointer
-ASFLAGS=-g -Wall -O2 -fdollars-in-identifiers -mmacosx-version-min=10.6 -D_DARWIN_USE_64_BIT_INODE -arch x86_64 -fno-omit-frame-pointer
-LINKFLAGS=-g -mmacosx-version-min=10.6 -Wl,-no_pie -arch x86_64 -dynamic -twolevel_namespace -bind_at_load -pagezero_size 0x100000
-LDFLAGS=
-__LDFLAGS__=
-LIBS=-lSystem -lc -ldl -lpthread -lz -lm
